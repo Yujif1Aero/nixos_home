ad0d85e12d1eb4abfdeab6584d2325f95045b6ca
Yuji Shimojima <yuji.shimojima.s@gmail.com>
2024-04-10 22:08:47 +0200

    delete chrome

--- gui/browser.nix
+++ gui/browser.nix
@@ -2,9 +2,9 @@
   programs = {
     firefox.enable = true;
     google-chrome.enable = true;
-    vivaldi = {
-      enable = true;
-      commandLineArgs = ["--enable-features=WebUIDarkMode" "--force-dark-mode"];
-    };
+    # vivaldi = {
+    #   enable = true;
+    #   commandLineArgs = ["--enable-features=WebUIDarkMode" "--force-dark-mode"];
+    # };
   };
 }
