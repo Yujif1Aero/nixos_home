e6e9bf12830be8b12cf699407bd027714c96fcc9
Yujif1Aero <yuji.shimojima.s@gmail.com>
2025-06-08 21:02:50 +0200

    gnumake, gcc, gdb delete

--- developmenttool/tool.nix
+++ developmenttool/tool.nix
@@ -1,10 +1,10 @@
 {pkgs, ...}: {
   home.packages = with pkgs; [
-    gcc13
-    gdb
+#    gcc13
+#    gdb
     texlive.combined.scheme-full
 #    python312
-    gnumake
+#    gnumake
     gnuplot
     paraview
     htop
